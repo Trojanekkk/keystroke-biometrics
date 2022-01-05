3834bf0d805ed5c5d29a2f128d079730d12d1e1f
Trojanekkk <W.Trojan27@gmail.com>
2022-01-05 15:00:13 +0100

    SERVER: Add DB driver; DB connection test

--- web/server/index.js
+++ web/server/index.js
@@ -1,13 +1,19 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const path = require('path')
+
 const app = express()
 const port = 3000
+const db_uri = 'mongodb://localhost:27017'
+
+// Connect to the DB
+const client = new MongoClient(db_uri)
 
 // CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');  
 next();
 })
 
@@ -20,6 +26,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Define controllers
 // Get text from db in specified lang
+async function getText () {
+  try {
+    await client.connect()
+
+    await client.db('admin').command({ ping: 1 })
+    console.log('Connected to the DB')
+  } finally {
+    await client.close()
+  }
+}
+getText().catch(console.dir)
 // Save results into database
 
 // Define routes
@@ -31,7 +48,8 @@ app.get(API_ROOT + '/text', (req, res) => {
 
 app.post(API_ROOT + '/results', (req, res) => {
   console.log(req.body)
-  res.send(`This is results received confirmation`)
+  res.send(true)
+  // if error in controller, return false
 })
 
 // Run server
