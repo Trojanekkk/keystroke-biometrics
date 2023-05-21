5f5d563f0760ddb9c89122ff73dc2e52a54feac7
Wojciech Trojanowski <W.Trojan27@gmail.com>
2023-05-22 00:55:51 +0200

    Fix small bugs in UI; Put max steps in var

--- web/client/components/MiniLab.vue
+++ web/client/components/MiniLab.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="mini_laboratory">
+    <h5><br>A few rules before you start</h5>
+    <p />
+    <p>
+      1. Before you start, read the whole text slowly and cerfully. Pay extra attention to spelling.
+    </p>
+    <p>
+      2. Of course, you can read the text during rewriting, the text will be visible during the whole experiment.
+    </p>
+    <p>
+      3. Do not be in a hurry with rewriting, try to do in natural manner, in you normal speed.
+    </p>
+    <p>
+      5. Do not correct mistakes (neither just ater you make it either when you finished particular step), do not worry about them, just continue rewriting and jump to the next step.
+    </p>
+    <p>
+      6. Do not forget to send the results as you finished.
+    </p>
+    <p>
+      7. Please do not refresh the page during the assesment - all results and data will be lost.
+    </p>
+    <h5><br>Rewrite given text to the field below</h5>
+    <p />
     <ValidationObserver v-slot="{ invalid }">
       <p>
         Name / Nickname (for identification) <span style="color: red">*</span><br>
@@ -12,21 +34,19 @@
         </ValidationProvider>
       </p>
       <p>
-        <b-form-input :disabled="true">
-          moja uczelnia jest spoko i studiuje na niej sporo studentów
-        </b-form-input>
+        <b-form-input :disabled="true" value="moja uczelnia jest spoko i studiuje na niej sporo studentów" />
       </p>
       <p>
         <b-form-textarea
           v-model="results.text"
           rows="10"
-          :disabled="archive.length > 2"
+          :disabled="archive.length > numberOfSteps - 1"
           @keydown="capturePressing($event)"
           @keyup="captureReleasing($event)"
         />
       </p>
       <p
-        v-if="archive.length < 3"
+        v-if="archive.length < numberOfSteps"
       >
         <br>
         <b-button
@@ -49,9 +69,9 @@
         </b-button>
       </p>
       <p>
-        <center>Finished steps {{ archive.length }} / 3</center>
+        <center>Finished steps {{ archive.length }} / {{ numberOfSteps }}</center>
         <br>
-        <b-progress style="background-color: white" :value="archive.length" max="3" variant="danger" animated />
+        <b-progress style="background-color: white" :value="archive.length" :max="numberOfSteps" variant="danger" animated />
       </p>
     </ValidationObserver>
   </div>
@@ -67,6 +87,7 @@ export default {
   },
   data () {
     return {
+      numberOfSteps: 3,
       userData: {
         nickname: ''
       },
@@ -96,6 +117,11 @@ export default {
     },
     nextStep () {
       this.archive.push(this.results)
+      this.results = {
+        text: '',
+        keydowns: [],
+        keyups: []
+      }
     },
     async sendResults () {
       const userAgent = navigator.userAgent
