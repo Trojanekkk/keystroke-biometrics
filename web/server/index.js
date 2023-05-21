c61dc10cbb1e360bf31f4e425dec32327587c78d
Wojciech Trojanowski <W.Trojan27@gmail.com>
2023-05-22 00:06:01 +0200

    Add server via TLS if certs are available

--- web/server/index.js
+++ web/server/index.js
@@ -2,6 +2,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const initDependencies = require("./config/init");
 const log = require("log-beautify");
 const path = require("path");
@@ -30,7 +33,43 @@ require("./routes/result.routes")(app);
 // Initialize application dependencies
 initDependencies.initStorage();
 
-// Run server
-app.listen(process.env.PORT, () => {
-  log.success(`Server is running: http://localhost:${process.env.PORT}`);
+// Config https and https servers
+const httpServer = http.createServer(app);
+
+// Run servers
+httpServer.listen(process.env.HTTP_PORT, () => {
+  console.log(
+    `HTTP Server is running: http://localhost:${process.env.HTTP_PORT}`
+  );
 });
+
+// Config HTTPS server if certs exist
+if (
+  fs.existsSync(process.env.CERT_PATH + "privkey.pem") &&
+  fs.existsSync(process.env.CERT_PATH + "cert.pem") &&
+  fs.existsSync(process.env.CERT_PATH + "fullchain.pem")
+) {
+  const privateKey = fs.readFileSync(
+    process.env.CERT_PATH + "privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    process.env.CERT_PATH + "cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(process.env.CERT_PATH + "fullchain.pem", "utf8");
+
+  const ca_creds = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+  };
+
+  const httpsServer = https.createServer(ca_creds, app);
+
+  httpsServer.listen(process.env.HTTPS_PORT, () => {
+    console.log(
+      `HTTPS Server is running https://localhost:${process.env.HTTPS_PORT}`
+    );
+  });
+}
